511962bcbf3f26a29b8f41b85e10b22649fd21ee
Damir R. Konkov <d4mk0f@gmail.com>
2013-11-05 02:43:03 +0400

    line.php was added

---
+++ line.php
@@ -0,0 +1,18 @@
+﻿<? include "blocks/connect_db.php" ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/><!--Установка кодировки страницы-->
+    <link rel='stylesheet' href="assets/styles/application.css" type='text/css'><!--Подключение теблицы стилей-->
+    <title>Линия | E-bets. Ставки у Евгена</title>
+  </head>
+  <body>
+    <div id="base_block">
+      <? include "blocks/header.php" ?>
+      <div id="content">
+        Вы находитесь в разделе "Линия". Здесь вы можете выбрать интересующие вас события и сделать на них ставки.
+      </div>
+      <? include "blocks/footer.php" ?>
+    </div>
+  </body>
+</html>
