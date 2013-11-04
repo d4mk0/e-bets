86b2a9c61831779fefe3c17eac7b384e769855cc
Damir R. Konkov <d4mk0f@gmail.com>
2013-11-05 02:43:38 +0400

    header was changed

--- blocks/header.php
+++ blocks/header.php
@@ -1,3 +1,20 @@
 <div id="header">
-  it is header
+  <?
+    if(isset($_COOKIE['bsecret'])) {
+      include "blocks/connect_db.php";
+      $query = sprintf("SELECT login, balance FROM users WHERE secret='%s'", $_COOKIE['bsecret']);
+      $result = mysql_query($query);
+      if(mysql_num_rows($result) == 0) $auth = false;
+      else {
+        $auth = true;
+        $row = mysql_fetch_array($result);
+        echo "<div class=\"text\">Ваш баланс: <strong>".$row['balance']."</strong> руб. Удачных ставок ".$row['login']."!</div>";
+      }
+    } else {
+      echo "<div class=\"autority\"><a href=\"autorization.php\">Авторизируйтесь</a> или <a href=\"register.php\">Зарегистрируйтесь</a></div>";
+    }
+  ?>
+  
+  <? include "blocks/menu.php" ?>
+  <div id="logo">Evgen BETS</div>
 </div>
